8b8e969e413347b30e3742ef8717d055421f6cb1
ryszardrecenzje-pixel <ryszardrecenzje@gmail.com>
2026-09-02 20:45:40 +0200

    Update tts.py

--- voice/tts.py
+++ voice/tts.py
@@ -1,1 +1,10 @@
+# voice/tts.py
+import pyttsx3
 
+engine = pyttsx3.init()
+engine.setProperty("rate", 180)
+engine.setProperty("volume", 1.0)
+
+def speak(text: str):
+    engine.say(text)
+    engine.runAndWait()
